80c9af0aaf508739c98713b513d08376b15898f8
M4rT1nZzZ <ricardomateusmartins@gmail.com>
2024-01-09 22:03:03 +0000

    commit inicial

---
+++ Base de Dados APP/src/main/java/com/ips/tpsi/pokemonapp/controller/WebController.java
@@ -0,0 +1,57 @@
+package com.ips.tpsi.pokemonapp.controller;
+
+import com.ips.tpsi.pokemonapp.bc.WebBc;
+import com.ips.tpsi.pokemonapp.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class WebController {
+
+    @Autowired
+    WebBc bc;
+
+    public WebController(WebBc bc) {
+        this.bc = bc;
+    }
+
+    @GetMapping("/home") // @GetMapping("/")
+    public ModelAndView getHome() {
+        ModelAndView mv = new ModelAndView("index");
+        // aceder à business component > acede ao repositorio > obtem dados
+
+        return mv;
+    }
+
+
+
+    @PostMapping("/login")
+    public ModelAndView login(String uname, String psw) {
+
+        // aceder à business component para validar o login
+        boolean isLoginValid = bc.isLoginValid(uname, psw);
+
+        // uma vez que o login é válido procedemos ao login
+        ModelAndView mv = new ModelAndView("index");
+        if (isLoginValid) {
+            // aceder à business component > acede ao repositorio > obtem dados
+            mv.addObject("loginValid", "O Login é válido.");
+        } else {
+            mv.addObject("loginValid", "O Login é inválido.");
+        }
+        return mv;
+    }
+
+
+    @GetMapping("/username")
+    public ModelAndView getUserByUsername(String username) {
+        User user = bc.getRepositoryUserInfoByUsername(username);
+        ModelAndView mv = new ModelAndView("index");
+        mv.addObject("user", user);
+        return mv;
+    }
+
+}
---
+++ Base de Dados APP/src/test/java/com/ips/tpsi/pokemonapp/PokemonwebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ips.tpsi.pokemonapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonwebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
